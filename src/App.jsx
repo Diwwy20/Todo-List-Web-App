ea9b16485f1e86320a1a4dc8429219bc5b61911b
Diwwy20 <ipureba2@gmail.com>
2025-04-28 18:49:07 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
 import SummaryPage from "./pages/SummaryPage";
 import MyTaskPage from "./pages/MyTaskPage";
 import MainLayout from "./layout/MainLayout";
 import "./locales";
 
 function App() {
+  const location = useLocation();
   return (
     <Routes>
       <Route element={<MainLayout />}>
@@ -12,6 +13,11 @@ function App() {
         <Route path="/summary" element={<SummaryPage />} />
         <Route path="/my-task" element={<MyTaskPage />} />
       </Route>
+
+      <Route
+        path="*"
+        element={<Navigate to="/" replace state={{ from: location }} />}
+      />
     </Routes>
   );
 }
